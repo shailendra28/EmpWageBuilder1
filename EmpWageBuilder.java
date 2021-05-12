d5d7616756eace930d775a1118902f0d84d1435b
shailendra28 <yshailendrakumar1768@gmail.com>
2021-05-13 01:50:05 +0530

    ADDED: Random Condition To Check Employee Is Present or Not

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,8 +1,15 @@
-
 public class EmpWageBuilder {
 
 	public static void main(String[] args) {
 		System.out.println("========> Welcome To The Employee Wage Computation Program <========");
+		// Condition to check Employee Attendance
+		int EmpCheck = 1;
+        double present = Math.floor(Math.random()*10) %2;
+        if (present==EmpCheck)
+        	System.out.println("Employee is present");
+        else
+        	
+        	System.out.println("Employee is abscent");
 
 	}
 
