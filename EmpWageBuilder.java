ca17917cbd0848a60af292703980204e68283a78
shailendra28 <yshailendrakumar1768@gmail.com>
2021-05-13 01:42:55 +0530

    Welcome To The Employee Wage Computation Program

---
+++ EmpWageBuilder.java
@@ -0,0 +1,9 @@
+
+public class EmpWageBuilder {
+
+	public static void main(String[] args) {
+		System.out.println("========> Welcome To The Employee Wage Computation Program <========");
+
+	}
+
+}
