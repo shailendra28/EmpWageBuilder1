c7b0e8f510a61688f7c1ad288edfb220eb5c7823
shailendra28 <yshailendrakumar1768@gmail.com>
2021-05-13 11:12:06 +0530

    ADDED: Ability to save wage of company

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,11 +1,19 @@
-
 public class EmpWageBuilder {
 	public static final  int IS_PART_TIME = 1;// Variable will be a compile time constant
     public static final int IS_FULL_TIME = 2;
-    //Created Method along with parameter
+    private int WAGE_PER_HOUR;
+	private int DAYS_PER_MONTH;
+	private int MAX_HOUR_MONTH;
+	//Created Method along with parameter
     //Put the while loop inside a method
-    public static int calculateTotalwage(String company, int WAGE_PER_HOUR, int  DAYS_PER_MONTH, int MAX_HOUR_MONTH)
+    public EmpWageBuilder(String company, int WAGE_PER_HOUR, int  DAYS_PER_MONTH, int MAX_HOUR_MONTH)
     { 
+    	this.WAGE_PER_HOUR =  WAGE_PER_HOUR;
+    	this. DAYS_PER_MONTH = DAYS_PER_MONTH;
+    	this.MAX_HOUR_MONTH = MAX_HOUR_MONTH;
+    }
+    public void computeEmpWage()
+    {
 	    int empHrs = 0;
         int totalWorkingDays = 0;
 	    int totalEmpHrs = 0;
@@ -22,7 +30,7 @@ public class EmpWageBuilder {
         case IS_FULL_TIME:	
         	empHrs = 8;
         	break;
-        	default:
+        default:
         	empHrs=0;
         	break;
         }
@@ -31,15 +39,14 @@ public class EmpWageBuilder {
         }
         int TotalWage = totalEmpHrs * WAGE_PER_HOUR;
         System.out.println("Total Employee wage of Company: " + TotalWage);
-        return TotalWage;
+        
 	    }
 	  }
-	    
-	    //Inside main method parameter work as input
 	    public static void main(String[] args) {
 		  System.out.println("========> Welcome To The Employee Wage Computation Program ");
-		  calculateTotalwage("DMart", 20, 20, 100);
-			calculateTotalwage("Reliance", 30, 15, 150);
+		  EmpWageBuilder DMart = new EmpWageBuilder("Dmart", 20,29,100);
+		  DMart.computeEmpWage();
+		  System.out.println(DMart);
         } 
 	     
   } 
